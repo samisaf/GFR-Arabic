47863f0ca6a9b0f426c8c315e6588a6eb33d97ee
Sami Safadi <samisaf@gmail.com>
2023-04-23 14:14:12 -0500

    adds input guards

--- src/app.jsx
+++ src/app.jsx
@@ -12,11 +12,14 @@ import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
 import { prefixer } from 'stylis';
 
+// Create rtl cache
+const cacheRtl = createCache({ key: 'muirtl', stylisPlugins: [prefixer, rtlPlugin], });
+
 let t1 = 'معادلة';
 let t2 = ' CKD-EPI ';
-let t3 = ' لتقدير الرشح الكبيبي الكلوي';
-let title = t1 + t2 + t3;
-let message = "أدخل بيانات المريض"
+let t3 = 'لتقدير الرشح الكبيبي الكلوي';
+let title = `${t1} ${t2} ${t3}`;
+let outputMessage = "أدخل بيانات المريض"
 let description = [
   'هذه الصفحة تستخدم المعادلة لعام ٢٠٢١',
   'تعتبر هذه المعادلة المعيار الموصى به الان',
@@ -24,30 +27,19 @@ let description = [
   'ينصح باستخدام هذه المعادلة للمرضى ذوي وظائف الكلى المستقرة',
 ];
 
-let card = "هذا البرنامج من إعداد الدكتور سامي صفدي. يمكن مراسلتنا بالنقر على هذا "
-
-
-// Create rtl cache
-const cacheRtl = createCache({
-  key: 'muirtl',
-  stylisPlugins: [prefixer, rtlPlugin],
-});
-
-function calcB(Scr, Gender) {
-  if (Gender == "Female" && Scr <= 0.7) return -0.241;
-  else if (Gender == "Female" && Scr > 0.7) return -1.2;
-  else if (Gender == "Male" && Scr <= 0.9) return -0.302;
-  else if (Gender == "Male" && Scr > 0.9) return -1.2;
-  else return 0;
-}
+let aboutMessage = "هذا البرنامج من إعداد الدكتور سامي صفدي. يمكن مراسلتنا بالنقر على هذا "
 
 function calcGFR(Scr, Age, Gender) {
-  let A = (Gender == "Female") ? 0.7 : 0.9
-  let B = calcB(Scr, Gender)
-  let temp1 = 142 * (Scr / A) ** B
-  let temp2 = 0.9938 ** Age
-  let temp3 = Gender == "Male" ? 1 : 1.012
-  return temp1 * temp2 * temp3
+  let A = (Gender == "Female") ? 0.7 : 0.9;
+  let B = null;
+  if (Gender == "Female" && Scr <= 0.7) B = -0.241;
+  else if (Gender == "Female" && Scr > 0.7) B = -1.2;
+  else if (Gender == "Male" && Scr <= 0.9) B = -0.302;
+  else if (Gender == "Male" && Scr > 0.9) B = -1.2;
+  let term1 = 142 * (Scr / A) ** B;
+  let term2 = 0.9938 ** Age;
+  let term3 = Gender == "Male" ? 1 : 1.012;
+  return term1 * term2 * term3
 }
 
 export function App() {
@@ -59,70 +51,69 @@ export function App() {
   let output1 = "معدل الرشح الكبيبي الكلوي"
   let output2 = " (ميلي ليتر | دقيقة | ١.٧٣ متر مربع)"
   let output3 = `${output1} ${GFR} ${output2}`
-  let output = Scr && Age && Gender && output3
+  let output = (Scr > 0) && (Age > 0) && Gender && output3
+
   return (
-    <>
-      <CacheProvider value={cacheRtl}>
+    <CacheProvider value={cacheRtl}>
+      <h1>{title}</h1>
+      <Alert severity="warning">
+        {description.map((i) => (
+          <p>{i}</p>
+        ))}
+      </Alert>
+      <br></br>
 
-        <h1>{title}</h1>
-        <Alert severity="warning">
-            {description.map((i) => (
-              <p>{i}</p>
-            ))}
-        </Alert>
-        <br></br>
+      <Alert severity="info">
+        <TextField
+          error={Scr <= 0 ? true : false}
+          id="serum-creatinine"
+          label="تركيز الكرياتينين (مغ|دسل)"
+          type="number"
+          value={Scr}
+          placeholder=''
+          onInput={e => setScr(e.target.value)}
+          variant="standard"
+          fullWidth
+        />
 
-        <Alert severity="info">
-          <TextField
-            id="outlined-number"
-            label="تركيز الكرياتينين (مغ|دسل)"
-            type="number"
-            value={Scr}
-            placeholder=''
-            onInput={e => setScr(e.target.value)}
-            variant="standard"
-            fullWidth
-          />
+        <TextField
+          id="age"
+          error={Age <= 0 ? true : false}
+          label="العمر (سنة)"
+          type="number"
+          value={Age}
+          placeholder=''
+          onInput={e => setAge(e.target.value)}
+          variant="standard"
+          fullWidth
+        />
 
-          <TextField
-            id="outlined-number"
-            label="العمر (سنة)"
-            type="number"
-            value={Age}
-            placeholder=''
-            onInput={e => setAge(e.target.value)}
-            variant="standard"
-            fullWidth
-          />
+        <p></p>
 
-          <p></p>
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">الجنس</FormLabel>
+          <RadioGroup
+            row
+            id="gender"
+            name="radio-buttons-group"
+            onInput={e => setGender(e.target.value)}
+          >
+            <FormControlLabel value="Female" control={<Radio />} label="امرأة" />
+            <FormControlLabel value="Male" control={<Radio />} label="رجل" />
+          </RadioGroup>
+        </FormControl>
+      </Alert>
+      <br></br>
 
-          <FormControl>
-            <FormLabel id="demo-radio-buttons-group-label">الجنس</FormLabel>
-            <RadioGroup
-              row
-              // defaultValue="Female"
-              name="radio-buttons-group"
-              onInput={e => setGender(e.target.value)}
-            >
-              <FormControlLabel value="Female" control={<Radio />} label="امرأة" />
-              <FormControlLabel value="Male" control={<Radio />} label="رجل" />
-            </RadioGroup>
-          </FormControl>
-        </Alert>
-        <br></br>
+      <Alert severity="success">
+        {output ? output : outputMessage}
+      </Alert>
 
-        <Alert severity="success">
-          {output ? output : message}
-        </Alert>
-
-        <br></br>
-        <Alert severity="error" >
-          {card}
-          <a href="mailto:samisaf@gmail.com">الرابط</a>
-        </Alert>
-      </CacheProvider>
-    </>
+      <br></br>
+      <Alert severity="error" >
+        {aboutMessage}
+        <a href="mailto:samisaf@gmail.com">الرابط</a>
+      </Alert>      </CacheProvider>
 
   )
 }
